2aac7b50e19d472a1940c37c43d203d4e5aa184a
Jose Perez-Roura <jose.perezroura@gmail.com>
2018-01-27 15:08:46 -0500

    new pages set up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  get 'static_pages/work'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'welcome#index'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get work" do
+    get static_pages_work_url
+    assert_response :success
+  end
+
 end
